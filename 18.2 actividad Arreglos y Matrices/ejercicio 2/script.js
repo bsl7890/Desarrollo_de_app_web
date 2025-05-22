270d0d34531f12b6595cf877770c6f1606f2eb37
bsl7890 <benjamin.santander.lopez.2016@gmail.com>
2025-05-22 14:45:06 +0200

    32

--- 18.2 actividad Arreglos y Matrices/ejercicio 2/script.js
+++ 18.2 actividad Arreglos y Matrices/ejercicio 2/script.js
@@ -1,6 +1,6 @@
 console.log("conectado correctamente...");
 
-// Ejercicio 2: Frase con número, verbo, adverbio, adjetivo y sustantivo
+// Ejercicio 2: Frases con números, verbo, adverbio, adjetivo y sustantivo
 function ejercicio2() {
     let datos = [3, "gatos", "negros", "duermen", "tranquilamente"];
     let frase = "Los " + datos[0] + " " + datos[2] + " " + datos[1] + " " + datos[3] + " " + datos[4] + ".";
